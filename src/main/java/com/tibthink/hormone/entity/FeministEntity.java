68ab8d59e4c48c066f337188c8810eb13a4159ea
tibthink <bent23jam@gmail.com>
2020-09-06 14:36:27 -0400

    karen entity

--- src/main/java/com/tibthink/hormone/entity/FeministEntity.java
+++ src/main/java/com/tibthink/hormone/entity/FeministEntity.java
@@ -1,6 +1,7 @@
 
 package com.tibthink.hormone.entity;
 
+import net.minecraftforge.registries.ObjectHolder;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.fml.network.NetworkHooks;
 import net.minecraftforge.fml.network.FMLPlayMessages;
@@ -15,36 +16,45 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraft.world.gen.Heightmap;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.World;
-import net.minecraft.world.ServerBossInfo;
-import net.minecraft.world.BossInfo;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.DamageSource;
 import net.minecraft.network.IPacket;
 import net.minecraft.item.SpawnEggItem;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.Item;
-import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.entity.projectile.AbstractArrowEntity;
+import net.minecraft.entity.monster.ZombieEntity;
 import net.minecraft.entity.monster.MonsterEntity;
 import net.minecraft.entity.ai.goal.SwimGoal;
+import net.minecraft.entity.ai.goal.RangedAttackGoal;
 import net.minecraft.entity.ai.goal.RandomWalkingGoal;
 import net.minecraft.entity.ai.goal.MeleeAttackGoal;
 import net.minecraft.entity.ai.goal.LookRandomlyGoal;
 import net.minecraft.entity.ai.goal.HurtByTargetGoal;
 import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.IRendersAsItem;
+import net.minecraft.entity.IRangedAttackMob;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.EntitySpawnPlacementRegistry;
 import net.minecraft.entity.EntityClassification;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.CreatureAttribute;
-import net.minecraft.client.renderer.entity.model.BipedModel;
-import net.minecraft.client.renderer.entity.layers.BipedArmorLayer;
-import net.minecraft.client.renderer.entity.BipedRenderer;
+import net.minecraft.client.renderer.entity.model.VillagerModel;
+import net.minecraft.client.renderer.entity.SpriteRenderer;
+import net.minecraft.client.renderer.entity.MobRenderer;
+import net.minecraft.client.Minecraft;
 
 import com.tibthink.hormone.itemgroup.HormoneItemGroup;
+import com.tibthink.hormone.item.EsterginSyringeItem;
 import com.tibthink.hormone.HormoneModElements;
 
 @HormoneModElements.ModElement.Tag
 public class FeministEntity extends HormoneModElements.ModElement {
 	public static EntityType entity = null;
+	@ObjectHolder("hormone:entitybulletfeminist")
+	public static final EntityType arrow = null;
 	public FeministEntity(HormoneModElements instance) {
 		super(instance, 22);
 		FMLJavaModLoadingContext.get().getModEventBus().register(this);
@@ -53,10 +63,13 @@ public class FeministEntity extends HormoneModElements.ModElement {
 	@Override
 	public void initElements() {
 		entity = (EntityType.Builder.<CustomEntity>create(CustomEntity::new, EntityClassification.MONSTER).setShouldReceiveVelocityUpdates(true)
-				.setTrackingRange(64).setUpdateInterval(3).setCustomClientFactory(CustomEntity::new).immuneToFire().size(0.6f, 1.8f))
+				.setTrackingRange(64).setUpdateInterval(3).setCustomClientFactory(CustomEntity::new).immuneToFire().size(0.6f, 1.95f))
 						.build("feminist").setRegistryName("feminist");
 		elements.entities.add(() -> entity);
 		elements.items.add(() -> new SpawnEggItem(entity, -1, -1, new Item.Properties().group(HormoneItemGroup.tab)).setRegistryName("feminist"));
+		elements.entities.add(() -> (EntityType.Builder.<ArrowCustomEntity>create(ArrowCustomEntity::new, EntityClassification.MISC)
+				.setShouldReceiveVelocityUpdates(true).setTrackingRange(64).setUpdateInterval(1).setCustomClientFactory(ArrowCustomEntity::new)
+				.size(0.5f, 0.5f)).build("entitybulletfeminist").setRegistryName("entitybulletfeminist"));
 	}
 
 	@Override
@@ -71,18 +84,17 @@ public class FeministEntity extends HormoneModElements.ModElement {
 	@SubscribeEvent
 	@OnlyIn(Dist.CLIENT)
 	public void registerModels(ModelRegistryEvent event) {
-		RenderingRegistry.registerEntityRenderingHandler(CustomEntity.class, renderManager -> {
-			BipedRenderer customRender = new BipedRenderer(renderManager, new BipedModel(), 0.5f) {
-				@Override
-				protected ResourceLocation getEntityTexture(Entity entity) {
-					return new ResourceLocation("hormone:textures/binder_layer_1.png");
-				}
-			};
-			customRender.addLayer(new BipedArmorLayer(customRender, new BipedModel(0.5f), new BipedModel(1)));
-			return customRender;
-		});
+		RenderingRegistry.registerEntityRenderingHandler(CustomEntity.class,
+				renderManager -> new MobRenderer(renderManager, new VillagerModel(0), 0.5f) {
+					@Override
+					protected ResourceLocation getEntityTexture(Entity entity1) {
+						return new ResourceLocation("hormone:textures/2020_08_27_mega-karen-15149791.png");
+					}
+				});
+		RenderingRegistry.registerEntityRenderingHandler(ArrowCustomEntity.class,
+				renderManager -> new SpriteRenderer(renderManager, Minecraft.getInstance().getItemRenderer()));
 	}
-	public static class CustomEntity extends MonsterEntity {
+	public static class CustomEntity extends ZombieEntity implements IRangedAttackMob {
 		public CustomEntity(FMLPlayMessages.SpawnEntity packet, World world) {
 			this(entity, world);
 		}
@@ -106,6 +118,12 @@ public class FeministEntity extends HormoneModElements.ModElement {
 			this.targetSelector.addGoal(3, new HurtByTargetGoal(this));
 			this.goalSelector.addGoal(4, new LookRandomlyGoal(this));
 			this.goalSelector.addGoal(5, new SwimGoal(this));
+			this.goalSelector.addGoal(1, new RangedAttackGoal(this, 1.25, 20, 10) {
+				@Override
+				public boolean shouldContinueExecuting() {
+					return this.shouldExecute();
+				}
+			});
 		}
 
 		@Override
@@ -128,6 +146,13 @@ public class FeministEntity extends HormoneModElements.ModElement {
 		}
 
 		@Override
+		public boolean attackEntityFrom(DamageSource source, float amount) {
+			if (source == DamageSource.LIGHTNING_BOLT)
+				return false;
+			return super.attackEntityFrom(source, amount);
+		}
+
+		@Override
 		protected void registerAttributes() {
 			super.registerAttributes();
 			if (this.getAttribute(SharedMonsterAttributes.MOVEMENT_SPEED) != null)
@@ -141,27 +166,48 @@ public class FeministEntity extends HormoneModElements.ModElement {
 			this.getAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(3);
 		}
 
-		@Override
-		public boolean isNonBoss() {
-			return false;
+		public void attackEntityWithRangedAttack(LivingEntity target, float flval) {
+			ArrowCustomEntity entityarrow = new ArrowCustomEntity(arrow, this, this.world);
+			double d0 = target.posY + (double) target.getEyeHeight() - 1.1;
+			double d1 = target.posX - this.posX;
+			double d3 = target.posZ - this.posZ;
+			entityarrow.shoot(d1, d0 - entityarrow.posY + (double) MathHelper.sqrt(d1 * d1 + d3 * d3) * 0.2F, d3, 1.6F, 12.0F);
+			world.addEntity(entityarrow);
 		}
-		private final ServerBossInfo bossInfo = new ServerBossInfo(this.getDisplayName(), BossInfo.Color.PINK, BossInfo.Overlay.PROGRESS);
-		@Override
-		public void addTrackingPlayer(ServerPlayerEntity player) {
-			super.addTrackingPlayer(player);
-			this.bossInfo.addPlayer(player);
+	}
+
+	@OnlyIn(value = Dist.CLIENT, _interface = IRendersAsItem.class)
+	private static class ArrowCustomEntity extends AbstractArrowEntity implements IRendersAsItem {
+		public ArrowCustomEntity(FMLPlayMessages.SpawnEntity packet, World world) {
+			super(arrow, world);
+		}
+
+		public ArrowCustomEntity(EntityType<? extends ArrowCustomEntity> type, World world) {
+			super(type, world);
+		}
+
+		public ArrowCustomEntity(EntityType<? extends ArrowCustomEntity> type, double x, double y, double z, World world) {
+			super(type, x, y, z, world);
+		}
+
+		public ArrowCustomEntity(EntityType<? extends ArrowCustomEntity> type, LivingEntity entity, World world) {
+			super(type, entity, world);
 		}
 
 		@Override
-		public void removeTrackingPlayer(ServerPlayerEntity player) {
-			super.removeTrackingPlayer(player);
-			this.bossInfo.removePlayer(player);
+		public IPacket<?> createSpawnPacket() {
+			return NetworkHooks.getEntitySpawningPacket(this);
 		}
 
 		@Override
-		public void updateAITasks() {
-			super.updateAITasks();
-			this.bossInfo.setPercent(this.getHealth() / this.getMaxHealth());
+		@OnlyIn(Dist.CLIENT)
+		public ItemStack getItem() {
+			return new ItemStack(EsterginSyringeItem.block, (int) (1));
+		}
+
+		@Override
+		protected ItemStack getArrowStack() {
+			return new ItemStack(EsterginSyringeItem.block, (int) (1));
 		}
 	}
 }
